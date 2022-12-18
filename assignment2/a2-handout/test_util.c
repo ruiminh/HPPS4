59ed89eded87b9046e24465feee94bf369d266d0
Ruimin Huang <cph-rh206@cphbusiness.dk>
2022-12-18 11:59:08 +0100

    ore tests

--- assignment2/a2-handout/test_util.c
+++ assignment2/a2-handout/test_util.c
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "io.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -9,27 +10,27 @@ int main(){
   double* b = malloc(5*sizeof(double));
   double* c = malloc(5*sizeof(double));
 
-  a[0] = 0;
-  a[1] = 0;
-  a[2] = 0;
-  a[3] = 0;
-  a[4] = 0;
+  a[0] = 8.99;
+  a[1] = 90;
+  a[2] = 4.77;
+  a[3] = 67;
+  a[4] = 99;
 
-  b[0] = 1;
-  b[1] = 1;
-  b[2] = 1;
-  b[3] = 1;
+  b[0] = 55;
+  b[1] = 88;
+  b[2] = 99.666;
+  b[3] = 16;
   b[4] = 1;
 
-  c[0] = 2;
-  c[1] = 2;
-  c[2] = 2;
+  c[0] = 23;
+  c[1] = 6666;
+  c[2] = 1222.2222;
   c[3] = 2;
   c[4] = 2;
 
-  printf("%f\n", distance(5, &a[0], &b[0]));
+  printf("%f\n", distance(5, a, b));
 
-  printf("%f\n", distance(5, &c[0], &b[0]));
+  printf("%f\n", distance(5, c, b));
 
   int* list = malloc(5*sizeof(int));
 
@@ -50,6 +51,68 @@ int main(){
     printf("%d ", list[i]);
   }
   printf("\n");
+
+  //use 5_5.points to test insert closest
+
+  FILE *f= fopen("5_5.points","r");
+  int n,d;
+  double *points = read_points(f,&n, &d);
+
+  double *query = malloc(5*sizeof(double));
+  query[0]=0.123;
+  query[1]= 0.677;
+  query[2]= 0.321;
+  query[3] = 0.874;
+  query[4] = 0.465;
+
+  int *closest = malloc(3*sizeof(int));
+  closest[0]=-1;
+  closest[1]=-1;
+  closest[2]=-1;
+
+  for(int i=0;i<3;i++){
+    printf("%d  ",closest[i]);}
+
+  printf("\n");
+
+  insert_if_closer(3, 5, points, closest, query, 0);
+  
+
+  for(int i=0;i<3;i++){
+    printf("%d",closest[i]);}
+  printf("\n");
+
+  insert_if_closer(3, 5, points, closest, query,1); 
+
+    for(int i=0;i<3;i++){
+      printf("%d  ",closest[i]);}
+
+    printf("\n");
+
+    insert_if_closer(3, 5, points, closest, query,2);
+
+    for(int i=0;i<3;i++){
+      printf("%d  ",closest[i]);}
+
+    printf("\n");
+  
+ insert_if_closer(3, 5, points, closest, query, 3);
+  
+
+ for(int i=0;i<3;i++){
+    printf("%d  ",closest[i]);}
+  printf("\n");
+
+  insert_if_closer(3, 5, points, closest, query, 2);
+  
+
+ for(int i=0;i<3;i++){
+    printf("%d  ",closest[i]);}
+  printf("\n");
+
+
+  insert_if_closer(3, 5, points, closest, query, 4);
+  for(int i=0;i<3;i++){
+    printf("%d  ",closest[i]);}
+  printf("\n");
 }
-
-
